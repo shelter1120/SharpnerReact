5b3ca6701e67ee5a273c8a8acdf7ece4ba5d0757
shelter1120 <shelterk1120>
2023-01-13 17:12:09 +0530

    task 7 : add hard code simply

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ExpenseList = () => {
+  return (
+    <div>
+        <div>ExpenseList</div>
+       <div>Food rs 100</div>
+       <div>petrol rs 100</div>
+       <h3>Food rs 100</h3>
+</div>
+
+  )
+}
