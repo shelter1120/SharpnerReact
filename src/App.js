8ff04040a67c3fd7c147d4067bab524d9bf96a03
shelter1120 <shelterk1120>
2023-01-13 23:34:55 +0530

    task 9)  passing data and add loop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ExpenseList } from "./components/ExpenseList";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
 
 function App() {
+  const expense = [
+    { date: 2023, title: "food", amount: "29$" },
+    { date: 2024, title: "gym", amount: "27$" },
+    { date: 2025, title: "shakes", amount: "29$" },
+    { date: 2026, title: "cafe", amount: "23$" },
+    { date: 2027, title: "food", amount: "30$" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expense items</h1>
+
+      {
+      expense.map((key) => (
+        <ExpenseList date={key.date} title={key.title} amount={key.amount} />
+      ))
+      }
+      {/* <ExpenseList date={expense[1].date} title={expense[1].title} amount={expense[1].amount} />  */}
     </div>
   );
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import  './ExpenseList.css'
 
-export const ExpenseList = () => {
+
+export const ExpenseList = (props) => {
+    
   return (
-    <div>
-        <div>ExpenseList</div>
-       <div>Food rs 100</div>
-       <div>petrol rs 100</div>
-       <h3>Food rs 100</h3>
+
+    <div className="expense-item">
+    
+       <div>{props.date}</div>
+       <div className="expense-item__description">
+       <h3>{props.title}</h3>
+       <div className="expense-item__price">{props.amount}</div>
+       </div>
 </div>
 
   )
