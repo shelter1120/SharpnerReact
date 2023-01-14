41fc893ddfd74a76564c8bd5276a6c4f585ad8d1
shelter1120 <shelterk1120>
2023-01-14 16:33:06 +0530

    task 10) spliting comp for reusable

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import  './ExpenseList.css'
-
+import React from "react";
+import "./ExpenseList.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseTitle from "./ExpenseTitle";
 
 export const ExpenseList = (props) => {
-    
   return (
-
     <div className="expense-item">
-    
-       <div>{props.date}</div>
-       <div className="expense-item__description">
-       <h3>{props.title}</h3>
-       <div className="expense-item__price">{props.amount}</div>
-       </div>
-</div>
-
-  )
-}
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <ExpenseTitle title={props.title}/>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ExpenseTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseTitle = (props) => {
+  return (
+    <div>
+    <h3>{props.title}</h3>
+
+    </div>
+  )
+}
+
+export default ExpenseTitle
