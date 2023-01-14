d0efce4b870dbf235157f110c646c315d8036c6c
shelter1120 <shelterk1120>
2023-01-14 19:05:45 +0530

    just organize file structure and add arrow fun.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { ExpenseList } from "./components/ExpenseList";
+import { ExpenseList } from "./components/expense/ExpenseList";
 import { keyboard } from "@testing-library/user-event/dist/keyboard";
 
 function App() {
   const expense = [
-    { date: 2023, title: "food", amount: "29$" },
-    { date: 2024, title: "gym", amount: "27$" },
-    { date: 2025, title: "shakes", amount: "29$" },
-    { date: 2026, title: "cafe", amount: "23$" },
-    { date: 2027, title: "food", amount: "30$" },
+    { date: new Date(2023,7,14), title: "food", amount: "29$" },
+    { date: new Date(2024,8,13), title: "gym", amount: "27$" },
+    { date: new Date(2025,9,13), title: "shakes", amount: "29$" },
+    { date: new Date(2026,10,15), title: "cafe", amount: "23$" },
+    { date: new Date(2025,12,17), title: "food", amount: "30$" },
   ];
 
   return (
