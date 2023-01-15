c2f2c689dc9e0c341355d94a52f29f00a22c786e
shelter1120 <shelterk1120>
2023-01-15 17:23:27 +0530

    AR task 3: update title and amount using State

--- src/components/expense/ExpenseList.js
+++ src/components/expense/ExpenseList.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseList.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseTitle from "./ExpenseTitle";
 import Card from "../UI/Card";
 
 export const ExpenseList = (props) => {
+   const [amount, setAmount] = useState(props.amount)
+   const [title,setTitle] = useState(props.title)
   const ClickHandle= ()=>{
-    alert('clicked handle')
+    setTitle('change')
   }
+
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <ExpenseTitle title={props.title}/>
+        <ExpenseTitle title={title}/>
         </div>
-        <div className="expense-item__price">{props.amount}</div>
-        <button onClick={ClickHandle}>Delete</button>
+        <div className="expense-item__price">{amount}</div>
+        <button onClick={ClickHandle}>Change title</button>
+        <button onClick={()=>setAmount('100$')}>Update amount</button>
       </Card>
     
   );
