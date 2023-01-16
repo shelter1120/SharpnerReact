d9c998ca199890769283c99bc86c82473288eeab
shelter1120 <shelterk1120>
2023-01-16 08:18:52 +0530

    AR Task 7 : add object for submiting details.

---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+const ExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState(false);
+  const [date, setDate] = useState("");
+
+  function getSubmitDetails(e) {
+    e.preventDefault();
+    const expenseData = {
+      title : title,
+      amount : amount,
+      date : new Date(Date)
+    }
+    console.log(expenseData)
+ }
+
+  return (
+    <div>
+      <form onSubmit={getSubmitDetails}>
+        <div>
+          <label>Expense title:</label>
+          <input type="text" onChange={(e) => setTitle(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Expense Amount:</label>
+          <input
+            type="number"
+            onChange={(e) => setAmount(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label>Expense Date:</label>
+          <input type="date" onChange={(e) => setDate(e.target.value)}></input>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ExpenseForm;
